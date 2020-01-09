3c6bfaa0111e119b8da21c96814f7f09d8994776
GEPEL <shota1130s2k@gmail.com>
2020-01-10 05:39:42 +0900

    Charge It! Submarineプロジェクト
    1/9日夜勤進捗
    ・プレイヤーのデザイン作成と操作系コンポーネントの追加
    ・破壊可能オブジェクトの作成（未完）
    ・その他Probuilderでオブジェクトの作成

---
+++ Assets/Scripts/CrashController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class CrashController : MonoBehaviour
+{
+    [SerializeField] GameObject m_crash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            Debug.Log("Crashed");
+            Debug.Log(other.contacts[0].point);
+            Debug.Log(other.gameObject.GetComponent<Rigidbody>().velocity);
+            if (gameObject.transform.parent != null)
+            {
+                gameObject.transform.parent = null;
+            }
+            transform.DOMove(gameObject.transform.position + Vector3.forward * 1000f, 1f);
+            //Instantiate(m_crash, other.contacts[0].point, m_crash.transform.rotation);
+        }
+    }
+}
